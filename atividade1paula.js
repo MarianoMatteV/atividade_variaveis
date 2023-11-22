b32249c7fed20c30333798382389f6a832fa1b03
MarianoMatteV <144505951+MarianoMatteV@users.noreply.github.com>
2023-11-22 16:34:28 -0300

    Add files via upload

---
+++ atividade1paula.js
@@ -0,0 +1,50 @@
+
+// Atividade 1
+
+// let nome = prompt("Digite seu nome:")
+// console.log(typeof nome)
+
+// let cidade = prompt("Digite sua cidade:")
+// console.log(typeof cidade)
+
+// console.log(`Olá, meu nome é ${nome} e moro em ${cidade}.`)
+
+
+// Atividade 2
+
+// let pergunta = prompt("Você gosta de sushi? ")
+// let pergunta2 = prompt("Você joga videogame? ")
+// let pergunta3 = prompt("Você já terminou o ensino médio? ")
+
+// console.log('Você gosta de sushi? ', pergunta)
+// console.log('Você joga videogame? ', pergunta2)
+// console.log('Você já terminou o ensino médio? ', pergunta3)
+
+// Atividade 3
+
+// let x = 4
+// let y = 7
+// let z
+
+
+// z = x
+// x = y
+// y = z
+
+// console.log("Troquei valores x = 7", x)
+// console.log("Troquei valores y = 4", y)
+
+
+// Atividade 4
+
+let numero = Number(prompt("Digite um número"))
+
+let numero2 = Number(prompt("Digite outro número"))
+
+let somar = numero+numero2
+
+console.log(somar)
+
+let multiplicar = numero*numero2
+
+console.log(multiplicar)
